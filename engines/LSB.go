86406b5da22be8fd321c2c17a667601fb4674208
Ben Vezzani <dotvezz@gmail.com>
2019-01-21 19:24:07 -0500

    renaming file and making interface params non-pointer

--- engines/LSB.go
+++ engines/LSB.go
@@ -6,12 +6,13 @@ var LSB = &lsbContainer{}
 
 type lsbContainer struct{}
 
-func (lsbContainer) Encode(i *image.Image, d *[]byte) (err error) {
+func (lsbContainer) Encode(i image.Image, d *[]byte) (err error) {
+	if i.
 	//TODO: Actually do something here
 	return
 }
 
-func (lsbContainer) Decode(i *image.Image, d *[]byte) (err error) {
+func (lsbContainer) Decode(i image.Image, d *[]byte) (err error) {
 	//TODO: Actually do something here
 	return
 }
